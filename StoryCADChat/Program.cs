da2aea69b46ed161bd725bb792ae8ce1799948d4
Jake <49795711+Rarisma@users.noreply.github.com>
2025-04-11 14:53:26 +0100

    Add StoryCAD Chat

---
+++ StoryCADChat/Program.cs
@@ -0,0 +1,51 @@
+﻿// Import packages
+
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using StoryCAD.Services.API;
+using StoryCAD.Services.IoC;
+
+// Set Model and Key
+var builder = Kernel.CreateBuilder().AddOpenAIChatCompletion("gpt-4o-mini", "API Key");
+
+// Build the kernel
+Kernel kernel = builder.Build();
+var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+BootStrapper.Initialise();
+
+// Add the StoryCAD SK plugin
+kernel.Plugins.AddFromType<SemanticKernelApi>("StoryCAD");
+
+// Enable planning
+OpenAIPromptExecutionSettings openAiPromptExecutionSettings = new()
+{
+    FunctionChoiceBehavior = FunctionChoiceBehavior.Auto()
+};
+
+// Create a history store the conversation
+var history = new ChatHistory();
+
+// Initiate a back-and-forth chat
+string? userInput;
+do
+{
+    // Collect user input
+    Console.Write("User > ");
+    userInput = Console.ReadLine();
+
+    // Add user input
+    history.AddUserMessage(userInput);
+
+    // Get the response from the AI
+    var result = await chatCompletionService.GetChatMessageContentAsync(
+        history,
+        executionSettings: openAiPromptExecutionSettings,
+        kernel: kernel);
+
+    // Print the results
+    Console.WriteLine("Assistant > " + result);
+
+    // Add the message from the agent to the chat history
+    history.AddMessage(result.Role, result.Content ?? string.Empty);
+} while (true);
