3f85d57ad3a180b41337aea69cf10d62f64b4cbc
Jake <49795711+Rarisma@users.noreply.github.com>
2025-04-22 19:37:47 +0100

    add api starter

---
+++ Blank Project/StoryCAD-API-Starter/Program.cs
@@ -0,0 +1,12 @@
+﻿using StoryCAD.Services.IoC;
+
+namespace StoryCAD_API_Starter;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        BootStrapper.Initialise();
+
+    }
+}
